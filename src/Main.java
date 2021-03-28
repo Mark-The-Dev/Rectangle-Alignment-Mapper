b7051d7f55ecba030c360e2895763597aad33b1e
Mark <SwiftTactics@gmail.com>
2021-03-27 23:37:47 -0400

    initialized Scanner in main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,6 +11,11 @@ public class Main {
         newMap.addRectangles(rec2, rec1);
 
         newMap.drawMap();
+
+    }
+
+    public static void command(){
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
